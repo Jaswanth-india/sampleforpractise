4f250f38be5435ea91e2b5433de3f047b16fcd0e
Jaswanth <jaswanth@ADITHs-Mac-mini.local>
2024-10-30 12:06:02 +0530

    error fixed

--- dist/Banner.js
+++ dist/Banner.js
@@ -9,6 +9,6 @@ function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default":
 function Banner(props) {
   return /*#__PURE__*/_react["default"].createElement("div", {
     style: props.style
-  }, "Hello ", props.bannerData.countryFlag, " Hey! It looks like you are from ", /*#__PURE__*/_react["default"].createElement("b", null, props.bannerData.country), ". We support Parity Purchasing Power, so if you need it, use code ", /*#__PURE__*/_react["default"].createElement("b", null, "\u201C", props.bannerData.couponCode, "\u201D"), " to get ", /*#__PURE__*/_react["default"].createElement("b", null, props.bannerData.discountPercentage, "%"), " off your subscription at checkout.");
+  }, "Hello ", props.bannerData && props.bannerData.countryFlag, " Hey! It looks like you are from ", /*#__PURE__*/_react["default"].createElement("b", null, props.bannerData && props.bannerData.country), ". We support Parity Purchasing Power, so if you need it, use code ", /*#__PURE__*/_react["default"].createElement("b", null, "\u201C", props.bannerData && props.bannerData.couponCode, "\u201D"), " to get ", /*#__PURE__*/_react["default"].createElement("b", null, props.bannerData && props.bannerData.discountPercentage, "%"), " off your subscription at checkout.");
 }
 var _default = exports["default"] = Banner;
--- src/Banner.js
+++ src/Banner.js
@@ -4,7 +4,7 @@ function Banner (props){
     
     return (
         <div style={props.style}>
-            Hello {props.bannerData.countryFlag} Hey! It looks like you are from <b>{props.bannerData.country}</b>. We support Parity Purchasing Power, so if you need it, use code <b>“{props.bannerData.couponCode}”</b> to get <b>{props.bannerData.discountPercentage}%</b> off your subscription at checkout.
+            Hello {props.bannerData && props.bannerData.countryFlag} Hey! It looks like you are from <b>{props.bannerData && props.bannerData.country}</b>. We support Parity Purchasing Power, so if you need it, use code <b>“{props.bannerData && props.bannerData.couponCode}”</b> to get <b>{props.bannerData && props.bannerData.discountPercentage}%</b> off your subscription at checkout.
         </div>
     )
 }
